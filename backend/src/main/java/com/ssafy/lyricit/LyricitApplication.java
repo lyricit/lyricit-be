431d8be8a49157e2d30c52d00e3b05d47533e4e2
takealook97 <takealook97@naver.com>
2024-03-07 14:32:55 +0900

    chore: build init

---
+++ backend/src/main/java/com/ssafy/lyricit/LyricitApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.lyricit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricitApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ssafy/lyricit/LyricitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.lyricit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
