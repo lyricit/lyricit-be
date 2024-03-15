459f13b234929cabc9a472b5e48eee780e5505fc
takealook97 <takealook97@naver.com>
2024-03-15 10:09:31 +0900

    chore: add mysql setting

--- backend/src/main/java/com/ssafy/lyricit/LyricitApplication.java
+++ backend/src/main/java/com/ssafy/lyricit/LyricitApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.lyricit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LyricitApplication {
 
 	public static void main(String[] args) {
--- backend/src/main/java/com/ssafy/lyricit/common/BaseEntity.java
+++ backend/src/main/java/com/ssafy/lyricit/common/BaseEntity.java
@@ -3,6 +3,14 @@ package com.ssafy.lyricit.common;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,22 +20,17 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
-	private UUID id;
+	@Id
+	private String id = UUID.randomUUID().toString();
+
+	@CreatedDate
+	@Column(columnDefinition = "datetime(0) default now(0)", nullable = false, updatable = false)
 	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(columnDefinition = "datetime(0) default now(0)", nullable = false)
 	private LocalDateTime updatedAt;
-
-	public void generateUUID() {
-		if (id == null) {
-			id = UUID.randomUUID();
-		}
-	}
-
-	public void setCreatedAtNow() {
-		this.createdAt = LocalDateTime.now();
-	}
-
-	public void setUpdatedAtNow() {
-		this.updatedAt = LocalDateTime.now();
-	}
 }
