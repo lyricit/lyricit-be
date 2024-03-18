8780ac654473155b0b30a745651e52470fc3b01f
takealook97 <takealook97@naver.com>
2024-03-18 13:10:05 +0900

    feat(room): add room dto
    - requestDto: came from client
    - roomDto: saved on redis
    - in,out: published to subscribers

---
+++ backend/src/main/java/com/ssafy/lyricit/room/dto/RoomDto.java
@@ -0,0 +1,53 @@
+package com.ssafy.lyricit.room.dto;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.ssafy.lyricit.member.dto.MemberInGameDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS)
+// on redis
+public class RoomDto {
+	private String name;
+	private String password;
+	private Long playerCount;
+	private Long playerLimit;
+	private Boolean isPlaying;
+	private Boolean isPublic;
+	private Long roundLimit;
+	private Long roundTime;
+	private List<MemberInGameDto> members;
+
+	public RoomInsideDto toInsideDto(String roomNumber) {
+		return RoomInsideDto.builder()
+			.roomNumber(roomNumber)
+			.name(name)
+			.roundLimit(roundLimit)
+			.roundTime(roundTime)
+			.isPublic(isPublic)
+			.members(members)
+			.build();
+	}
+
+	public RoomOutsideDto toOutsideDto(String roomNumber) {
+		return RoomOutsideDto.builder()
+			.roomNumber(roomNumber)
+			.name(name)
+			.isPlaying(isPlaying)
+			.isPublic(isPublic)
+			.playerCount(playerCount)
+			.playerLimit(playerLimit)
+			.build();
+	}
+}
