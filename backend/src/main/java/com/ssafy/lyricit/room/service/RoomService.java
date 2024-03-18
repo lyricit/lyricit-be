50be085c438e9e204f5b7aa439ed5c61aaa7fd37
takealook97 <takealook97@naver.com>
2024-03-18 17:57:02 +0900

    refactor(member): add delete method on member service
    1. create and delete method returns global event response to subscribers.
    2. add logger for debugging

--- backend/src/main/java/com/ssafy/lyricit/room/service/RoomService.java
+++ backend/src/main/java/com/ssafy/lyricit/room/service/RoomService.java
@@ -6,17 +6,20 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.ssafy.lyricit.common.GlobalEventResponse;
+import com.ssafy.lyricit.common.type.EventType;
 import com.ssafy.lyricit.exception.BaseException;
 import com.ssafy.lyricit.member.domain.Member;
 import com.ssafy.lyricit.member.repository.MemberRepository;
 import com.ssafy.lyricit.room.domain.Room;
 import com.ssafy.lyricit.room.dto.RoomDto;
-import com.ssafy.lyricit.room.dto.RoomInsideDto;
 import com.ssafy.lyricit.room.dto.RoomOutsideDto;
 import com.ssafy.lyricit.room.dto.RoomRequestDto;
 import com.ssafy.lyricit.room.repository.RoomRepository;
@@ -31,30 +34,33 @@ public class RoomService {
 	private final RoomRepository roomRepository;
 	private final MemberRepository memberRepository;
 	private final SimpMessagingTemplate template; // 특정 Broker로 메세지를 전달
+	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	public void createRoom(String memberId, RoomRequestDto roomRequest) {
+	public String createRoom(String memberId, RoomRequestDto roomRequest) {
 		// to mysql
 		Member member = memberRepository.findById(memberId).orElseThrow(() -> new BaseException(MEMBER_NOT_FOUND));
 		Room room = roomRequest.toEntity(member);
-		roomRepository.save(room);// mysql에 저장
+		roomRepository.save(room);
 
 		// to redis
-		RoomDto roomDto = room.toDto(member.toInGameDto());// for redis
-		String roomNumber = findEmptyRoomNumber();// 기반으로 redis에 저장
-		roomRedisTemplate.opsForValue().set(roomNumber, roomDto);// redis에 저장
+		RoomDto roomDto = room.toDto(member.toInGameDto());
+		String roomNumber = findEmptyRoomNumber();
+		roomRedisTemplate.opsForValue().set(roomNumber, roomDto);
 
-		// pub
-		RoomInsideDto roomInsideDto = roomDto.toInsideDto(roomNumber);
+		log.info("\n [방 생성 완료] \n== mysql 저장 ==\n {} \n", room);
+		log.info("\n== redis 저장 == \n [{}번 방] \n {}", roomNumber, roomDto);
+
 		RoomOutsideDto roomOutsideDto = roomDto.toOutsideDto(roomNumber);
 
-		// pub to room
-		template.convertAndSend("/sub/rooms" + roomInsideDto.roomNumber(), roomInsideDto);
-
 		// pub to lounge
-		template.convertAndSend("/sub/lounge", roomOutsideDto); // 새로 추가된 방만 publish
+		template.convertAndSend("/sub/lounge",
+			GlobalEventResponse.builder()
+				.type(EventType.CREATED.name())
+				.data(roomOutsideDto));
+		return roomNumber;
 	}
 
-	public void readAllRooms() {// 라운지 접근 시 단 한번 호출
+	public List<RoomOutsideDto> readAllRooms() {// 라운지 접근 시 단 한번 호출
 		Set<String> roomNumbers = roomRedisTemplate.keys("*"); // get all keys
 		List<RoomOutsideDto> roomOutsideDtoList = new ArrayList<>();
 
@@ -68,10 +74,11 @@ public class RoomService {
 			}
 		}
 
-		template.convertAndSend("/sub/lounge", roomOutsideDtoList);
+		log.info("\n [모든 방 조회 결과] \n {}", roomOutsideDtoList);
+		return roomOutsideDtoList;
 	}
 
-	public void readRoomByRoomNumber(String roomNumber) {
+	public RoomOutsideDto readRoomByRoomNumber(String roomNumber) {
 		if (Boolean.FALSE.equals(roomRedisTemplate.hasKey(roomNumber))) {
 			throw new BaseException(ROOM_NOT_FOUND);
 		}
@@ -79,10 +86,32 @@ public class RoomService {
 		RoomOutsideDto roomOutsideDto =
 			((RoomDto)roomRedisTemplate.opsForValue().get(roomNumber)).toOutsideDto(roomNumber);
 
-		template.convertAndSend("/sub/rooms/" + roomNumber, roomOutsideDto);// 본인한테만 보내도록 설정
+		log.info("\n [방 조회 결과] \n {}", roomOutsideDto);
+		return roomOutsideDto;
 	}
 
 	// 방 삭제 메서드
+	public void deleteRoomByRoomNumber(String roomNumber) {
+		if (Boolean.FALSE.equals(roomRedisTemplate.hasKey(roomNumber))) {
+			throw new BaseException(ROOM_NOT_FOUND);
+		}
+		RoomDto roomDto = (RoomDto)roomRedisTemplate.opsForValue().get(roomNumber); // todo: add null exception
+
+		// delete from mysql
+		Room room = roomRepository.findById(roomDto.getRoomId()).orElseThrow(() -> new BaseException(ROOM_NOT_FOUND));
+		room.setDeleted(true); // soft deletion
+
+		// delete from redis
+		roomRedisTemplate.delete(roomNumber);
+
+		log.info("\n [방 삭제 완료] : {}", roomNumber);
+
+		// pub to lounge -> client do delete job
+		template.convertAndSend("/sub/lounge",
+			GlobalEventResponse.builder()
+				.type(EventType.DELETED.name())
+				.data(roomDto.toOutsideDto(roomNumber)));
+	}
 
 	private String findEmptyRoomNumber() {
 		long roomNumber = 1L;
