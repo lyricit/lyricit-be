21124208a882ca7e2b9345bfb90960e900966f09
takealook97 <takealook97@naver.com>
2024-03-21 16:59:20 +0900

    chore: add swagger description

--- backend/src/main/java/com/ssafy/lyricit/member/controller/MemberController.java
+++ backend/src/main/java/com/ssafy/lyricit/member/controller/MemberController.java
@@ -1,30 +1,46 @@
 package com.ssafy.lyricit.member.controller;
 
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-import com.ssafy.lyricit.member.aspect.MemberIdCheck;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.lyricit.member.dto.MemberDto;
+import com.ssafy.lyricit.member.dto.MemberIdDto;
+import com.ssafy.lyricit.member.dto.MemberRequestDto;
 import com.ssafy.lyricit.member.service.MemberService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
-@Controller
+@Tag(name = "로그인 컨트롤러", description = "로그인 및 회원가입 관련 기능이 포함되어 있음")
+@RestController
 @RequiredArgsConstructor
+@RequestMapping("/v1/members")
 public class MemberController {
 	private final MemberService memberService;
-	private final SimpMessagingTemplate template; //특정 Broker로 메세지를 전달
 
-	@MemberIdCheck
-	@MessageMapping("/members/{memberId}")
-	public void getMember(@DestinationVariable String memberId) {
-		template.convertAndSend("/sub/lounge", memberService.findMemberById(memberId));
+	@Operation(summary = "로그인 or 회원가입")
+	@PostMapping("/login")
+	public ResponseEntity<MemberIdDto> login(@RequestBody MemberRequestDto memberRequestDto) {
+		return ResponseEntity.ok(memberService.enterLounge(memberRequestDto));
 	}
 
-	@MemberIdCheck
-	@MessageMapping("/members")
-	public void getAllMembers() {
-		template.convertAndSend("/sub/lounge", memberService.findAllMembers());
+	@Operation(summary = "회원정보 조회 (테스트)")
+	@GetMapping("/{memberId}")
+	public ResponseEntity<MemberDto> getMember(@PathVariable String memberId) {
+		return ResponseEntity.ok(memberService.findMemberById(memberId));
+	}
+
+	@Operation(summary = "회원정보 전체 조회 (테스트)")
+	@GetMapping
+	public ResponseEntity<List<MemberDto>> getAllMembers() {
+		return ResponseEntity.ok(memberService.findAllMembers());
 	}
 }
