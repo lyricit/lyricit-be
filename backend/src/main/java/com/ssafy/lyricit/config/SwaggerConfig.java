e2780ca064eaa2ef08b7a1d51389fbb33e82a895
takealook97 <takealook97@naver.com>
2024-03-25 17:56:31 +0900

    fix: resolve swagger CORS error

--- backend/src/main/java/com/ssafy/lyricit/config/SwaggerConfig.java
+++ backend/src/main/java/com/ssafy/lyricit/config/SwaggerConfig.java
@@ -6,12 +6,14 @@ import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 import lombok.RequiredArgsConstructor;
 
 @OpenAPIDefinition(
 	info = @Info(title = "LYRIC:IT (리릭잇)",
 		description = "LYRIC:IT api document",
-		version = "v1"))
+		version = "v1"),
+	servers = {@Server(url = "https://api-dev.lyricit.site", description = "Default Server URL")})
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
