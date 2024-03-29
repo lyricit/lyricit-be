593669c9e9d90e67e15b711d1f1e736c217735ec
takealook97 <takealook97@naver.com>
2024-03-29 13:30:44 +0900

    feat: add subscribe unsubscribe event
    separate method and add roomNumber on stomp header

--- backend/src/main/java/com/ssafy/lyricit/config/ChannelInboundInterceptor.java
+++ backend/src/main/java/com/ssafy/lyricit/config/ChannelInboundInterceptor.java
@@ -15,6 +15,7 @@ import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 
 import com.ssafy.lyricit.config.events.LoungeEvent;
+import com.ssafy.lyricit.config.events.RoomEvent;
 import com.ssafy.lyricit.exception.BaseException;
 import com.ssafy.lyricit.member.dto.MemberOnlineDto;
 import com.ssafy.lyricit.member.repository.MemberRepository;
@@ -27,34 +28,57 @@ public class ChannelInboundInterceptor implements ChannelInterceptor {
 	private final MemberRepository memberRepository;
 	private final RedisTemplate<String, String> memberRedisTemplate;
 	private final ApplicationEventPublisher eventPublisher;
+	private final String LOUNGE_STR = "lounge", LOUNGE = "0";
 
 	@Override
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
 		StompHeaderAccessor header = StompHeaderAccessor.wrap(message);
 		if (StompCommand.CONNECT.equals(header.getCommand())) {
-			// add online member to redis template
-			String memberId = header.getFirstNativeHeader(MEMBER_ID.getValue());
-			if (memberId == null || memberId.isBlank()) {
-				throw new BaseException(MEMBER_ID_NOT_FOUND);
+			connect(header);
+		} else if (StompCommand.SUBSCRIBE.equals(header.getCommand())) {
+			subscribe(header);
+		}
+
+		return message;
+	}
+
+	@Override
+	public void afterSendCompletion(Message<?> message, MessageChannel channel, boolean sent, Exception ex) {
+		StompHeaderAccessor header = StompHeaderAccessor.wrap(message);
+		if (StompCommand.DISCONNECT.equals(header.getCommand())) {
+			disconnect(header);
+		} else if (StompCommand.UNSUBSCRIBE.equals(header.getCommand())) {
+			// if already disconnected
+			if (header.getSessionAttributes().get(MEMBER_ID.getValue()) == null) {
+				return;
 			}
 
-			String nickname = memberRepository.findById(memberId)
-				.orElseThrow(() -> new BaseException(MEMBER_NOT_FOUND)).getNickname();
-
-			memberRedisTemplate.opsForValue().set(memberId, nickname);
-
-			handleConnectCommand(header);
-
-			// publish to lounge
-			eventPublisher.publishEvent(new LoungeEvent(this,
-				MemberOnlineDto.builder()
-					.memberId(memberId)
-					.nickname(nickname)
-					.build()
-				, true
-			));
+			unsubscribe(header);
 		}
-		return message;
+	}
+
+	private void connect(StompHeaderAccessor header) {
+		// add online member to redis template
+		String memberId = header.getFirstNativeHeader(MEMBER_ID.getValue());
+		if (memberId == null || memberId.isBlank()) {
+			throw new BaseException(MEMBER_ID_NOT_FOUND);
+		}
+
+		String nickname = memberRepository.findById(memberId)
+			.orElseThrow(() -> new BaseException(MEMBER_NOT_FOUND)).getNickname();
+
+		memberRedisTemplate.opsForValue().set(memberId, nickname);
+
+		handleConnectCommand(header);
+
+		// publish to lounge
+		eventPublisher.publishEvent(new LoungeEvent(this,
+			MemberOnlineDto.builder()
+				.memberId(memberId)
+				.nickname(nickname)
+				.build()
+			, true
+		));
 	}
 
 	private void handleConnectCommand(StompHeaderAccessor header) {
@@ -66,34 +90,68 @@ public class ChannelInboundInterceptor implements ChannelInterceptor {
 		}
 
 		attributes.put(MEMBER_ID.getValue(), memberId);
+		attributes.put(ROOM_NUMBER.getValue(), LOUNGE);
 		header.setSessionAttributes(attributes);
 	}
 
-	@Override
-	public void afterSendCompletion(Message<?> message, MessageChannel channel, boolean sent, Exception ex) {
-		StompHeaderAccessor header = StompHeaderAccessor.wrap(message);
-		if (StompCommand.DISCONNECT.equals(header.getCommand())) {
-			// remove online member from redis template
-			String memberId = header.getSessionAttributes().get(MEMBER_ID.getValue()).toString();
-			if (memberId != null && !memberId.isBlank() && Boolean.TRUE.equals(memberRedisTemplate.hasKey(memberId))) {
-				// return;
-				String nickname = memberRedisTemplate.opsForValue().get(memberId);
-				// remove from redis
-				memberRedisTemplate.delete(memberId);
+	private void subscribe(StompHeaderAccessor header) {
+		String memberId = header.getSessionAttributes().get(MEMBER_ID.getValue()).toString();
+		String destination = header.getDestination();
+		if (destination == null) {
+			throw new BaseException(DESTINATION_NOT_FOUND);
+		}
+		String path = destination.substring(destination.lastIndexOf('/') + 1);
 
-				// publish to lounge
-				eventPublisher.publishEvent(new LoungeEvent(this,
-					MemberOnlineDto.builder()
-						.memberId(memberId)
-						.nickname(nickname)
-						.build()
-					, false
-				));
-
-				return;
-			}
-
+		if (memberId == null || memberId.isBlank()) {
 			throw new BaseException(MEMBER_ID_NOT_FOUND);
 		}
+
+		// set roomNumber
+		header.getSessionAttributes().put(ROOM_NUMBER.getValue(), path.equals(LOUNGE_STR) ? LOUNGE : path);
+	}
+
+	private void disconnect(StompHeaderAccessor header) {
+		// remove online member from redis template
+		String memberId = header.getSessionAttributes().get(MEMBER_ID.getValue()).toString();
+		String destination = header.getDestination();
+		if (destination == null) {
+			throw new BaseException(DESTINATION_NOT_FOUND);
+		}
+		String path = destination.substring(destination.lastIndexOf('/') + 1);
+
+		if (memberId == null || memberId.isBlank() || Boolean.FALSE.equals(memberRedisTemplate.hasKey(memberId))) {
+			throw new BaseException(MEMBER_ID_NOT_FOUND);
+		}
+
+		// if room member disconnect
+		if (!path.equals(LOUNGE_STR)) {
+			// publish room status
+			eventPublisher.publishEvent(new RoomEvent(this, memberId, path)); // todo: check if roomNumber is correct
+		}
+
+		// to lounge
+		String nickname = memberRedisTemplate.opsForValue().get(memberId);
+		// remove from redis
+		memberRedisTemplate.delete(memberId);
+
+		// publish to lounge
+		eventPublisher.publishEvent(new LoungeEvent(this,
+			MemberOnlineDto.builder()
+				.memberId(memberId)
+				.nickname(nickname)
+				.build()
+			, false
+		));
+	}
+
+	private void unsubscribe(StompHeaderAccessor header) {
+		String memberId = header.getSessionAttributes().get(MEMBER_ID.getValue()).toString();
+		String roomNumber = header.getSessionAttributes().get(ROOM_NUMBER.getValue()).toString();
+		if (memberId == null || memberId.isBlank()) {
+			throw new BaseException(MEMBER_ID_NOT_FOUND);
+		}
+
+		// publish room status to lounge
+		eventPublisher.publishEvent(new RoomEvent(this, memberId, roomNumber));
 	}
 }
