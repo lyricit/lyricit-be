183b4726cdee212cebacf36ea9dc3d799c308b12
takealook97 <takealook97@naver.com>
2024-03-29 16:33:41 +0900

    feat: add Async function to handle parallel disconnection

--- backend/src/main/java/com/ssafy/lyricit/LyricitApplication.java
+++ backend/src/main/java/com/ssafy/lyricit/LyricitApplication.java
@@ -3,9 +3,11 @@ package com.ssafy.lyricit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync(proxyTargetClass = true)
 public class LyricitApplication {
 
 	public static void main(String[] args) {
