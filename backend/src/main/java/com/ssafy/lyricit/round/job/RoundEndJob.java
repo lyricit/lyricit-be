42897f86812a383d21098c96237a66640f014b1e
takealook97 <takealook97@naver.com>
2024-03-31 20:54:07 +0900

    fix: set round job to use autowired bean through factory

---
+++ backend/src/main/java/com/ssafy/lyricit/round/job/RoundEndJob.java
@@ -0,0 +1,24 @@
+package com.ssafy.lyricit.round.job;
+
+import static com.ssafy.lyricit.game.type.JobNameConstant.*;
+
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RoundEndJob implements Job {
+	@Autowired
+	private RoundService roundService;
+
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+
+		JobDataMap dataMap = context.getJobDetail().getJobDataMap();
+		String roomNumber = dataMap.getString(ROOM_NUMBER.getValue());
+
+		roundService.endRound(roomNumber);
+	}
+}
---
+++ backend/src/main/java/com/ssafy/lyricit/round/job/RoundStartJob.java
@@ -0,0 +1,27 @@
+package com.ssafy.lyricit.round.job;
+
+import static com.ssafy.lyricit.game.type.JobNameConstant.*;
+
+import org.quartz.Job;
+import org.quartz.JobDataMap;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.ssafy.lyricit.game.dto.GameDto;
+
+@Component
+public class RoundStartJob implements Job {
+	@Autowired
+	private RoundService roundService;
+
+	@Override
+	public void execute(JobExecutionContext context) throws JobExecutionException {
+		JobDataMap dataMap = context.getJobDetail().getJobDataMap();
+		String roomNumber = dataMap.getString(ROOM_NUMBER.getValue());
+		GameDto gameDto = roundService.validateGame(roomNumber);
+		roundService.initRound(roomNumber, gameDto);
+	}
+}
+
