58cd8a9a6bb22ed0f960b3a5778f9a3bb7c2878f
takealook97 <takealook97@naver.com>
2024-03-31 18:36:57 +0900

    fix: set game chat message mapping method to chat controller

--- backend/src/main/java/com/ssafy/lyricit/chat/controller/ChatController.java
+++ backend/src/main/java/com/ssafy/lyricit/chat/controller/ChatController.java
@@ -1,11 +1,14 @@
 package com.ssafy.lyricit.chat.controller;
 
+import org.quartz.SchedulerException;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
 import com.ssafy.lyricit.chat.dto.LoungeChatRequestDto;
 import com.ssafy.lyricit.chat.dto.RoomChatRequestDto;
 import com.ssafy.lyricit.chat.service.ChatService;
+import com.ssafy.lyricit.game.dto.GameChatDto;
+import com.ssafy.lyricit.game.service.GameChatService;
 import com.ssafy.lyricit.member.aspect.MemberIdCheck;
 
 import lombok.RequiredArgsConstructor;
@@ -14,10 +17,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChatController {
 	private final ChatService chatService;
+	private final GameChatService gameChatService;
 
-	// Client가 SEND할 수 있는 경로
-	// stompConfig에서 설정한 applicationDestinationPrefixes와 @MessageMapping 경로가 병합됨
-	// "/pub/chat/enter"
 	@MemberIdCheck
 	@MessageMapping("/chat/enter")
 	public void enter(RoomChatRequestDto chatRequest) {
@@ -40,5 +41,10 @@ public class ChatController {
 	@MessageMapping("/chat/room")
 	public void chatToRoom(RoomChatRequestDto chatRequest) {
 		chatService.sendRoomChatMessage(chatRequest);
+	}
+
+	@MessageMapping("/chat/game")
+	public void chatToGame(GameChatDto chatRequest) throws SchedulerException {
+		gameChatService.checkGameChatMessage(chatRequest);
 	}
 }
