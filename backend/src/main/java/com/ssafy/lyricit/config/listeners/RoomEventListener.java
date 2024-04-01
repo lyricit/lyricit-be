154ec3afe2734ef896ed2334f100b287097db319
takealook97 <takealook97@naver.com>
2024-04-01 16:20:23 +0900

    fix(round): remove member from game if member disconnected while playing

--- backend/src/main/java/com/ssafy/lyricit/config/listeners/RoomEventListener.java
+++ backend/src/main/java/com/ssafy/lyricit/config/listeners/RoomEventListener.java
@@ -1,10 +1,13 @@
 package com.ssafy.lyricit.config.listeners;
 
 import org.springframework.context.event.EventListener;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import com.ssafy.lyricit.config.events.RoomEvent;
+import com.ssafy.lyricit.game.dto.GameDto;
 import com.ssafy.lyricit.room.service.RoomService;
+import com.ssafy.lyricit.round.service.RoundService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,9 +15,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class RoomEventListener {
 	private final RoomService roomService;
+	private final RoundService roundService;
+	private final RedisTemplate<String, GameDto> gameRedisTemplate;
 
 	@EventListener
 	public void handleRoomEvent(RoomEvent event) {
-		roomService.exitRoom(event.getMemberId(), event.getRoomNumber());
+		String roomNumber = event.getRoomNumber();
+		if (roomService.validateRoom(roomNumber).getIsPlaying()) {
+			GameDto gameDto = roundService.validateGame(roomNumber);
+			gameDto.getMembers().removeIf(scoreDto -> scoreDto.getMemberId().equals(event.getMemberId()));
+			gameRedisTemplate.opsForValue().set(roomNumber, gameDto);
+		}
+
+		roomService.exitRoom(event.getMemberId(), roomNumber);
 	}
 }
