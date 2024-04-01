23a55a93b8022f56eb0eece9289b944f0c5e2a39
takealook97 <takealook97@naver.com>
2024-04-01 20:28:56 +0900

    chore: set thread pool task scheduler for heartbeat

--- backend/src/main/java/com/ssafy/lyricit/config/StompWebSocketConfig.java
+++ backend/src/main/java/com/ssafy/lyricit/config/StompWebSocketConfig.java
@@ -1,8 +1,11 @@
 package com.ssafy.lyricit.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -20,8 +23,18 @@ public class StompWebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	@Override
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.setApplicationDestinationPrefixes("/pub");
-		registry.enableSimpleBroker("/sub").setHeartbeatValue(new long[] {4000, 4000});
-		;
+		registry.enableSimpleBroker("/sub")
+			.setHeartbeatValue(new long[] {4000, 4000})
+			.setTaskScheduler(taskScheduler());
+	}
+
+	@Bean
+	public TaskScheduler taskScheduler() {
+		ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+		scheduler.setPoolSize(10);
+		scheduler.setThreadNamePrefix("scheduled-task-");
+		scheduler.initialize();
+		return scheduler;
 	}
 
 	@Override
